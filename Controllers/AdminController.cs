7e38c8e4ec93e3ef808619904f2438bb1a8f442e
darren <764190362@qq.com>
2017-10-06 12:21:15 +0800

    添加了一个用户

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AspNetCoreIdentityExamples.Models;
 using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -22,6 +23,41 @@ namespace AspNetCoreIdentityExamples.Controllers
         public IActionResult Index()
         {
             return View(userManager.Users);
+        }
+
+
+        public ViewResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                AppUser user = new AppUser
+                {
+                    UserName = model.Name,
+                    Email = model.Email
+                    
+                };
+
+                //UserManager为我们准备了添加用户的异步方法，但秘密不是放在AppUser中的，而是放在添加方法CreateAsync中的
+                //用户添加的结果就是IdentityResult
+                IdentityResult result = await userManager.CreateAsync(user, model.Password);
+
+                //根据IdentityResult来判断是否添加用户成功
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }else
+                {
+                    //如果IdentitResult不成功，就可以获取IdentityError这个类型
+                    foreach(IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(model);
         }
     }
 }

71702b694620c7da3b2c185518d4854b7e7e495b
darren <764190362@qq.com>
2017-10-06 11:58:50 +0800

    展示所有用户

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreIdentityExamples.Models;
+using Microsoft.AspNetCore.Identity;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspNetCoreIdentityExamples.Controllers
+{
+    public class AdminController : Controller
+    {
+        private UserManager<AppUser> userManager;
+
+        public AdminController(UserManager<AppUser> userMgr)
+        {
+            userManager = userMgr;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(userManager.Users);
+        }
+    }
+}

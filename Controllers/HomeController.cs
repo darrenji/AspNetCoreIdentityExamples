b753d1f3402825645fa4ba0bb44e7ce939ce09a7
darren <764190362@qq.com>
2017-10-06 14:44:12 +0800

    登录

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using AspNetCoreIdentityExamples.Models;
+using Microsoft.AspNetCore.Identity;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,7 +13,10 @@ namespace AspNetCoreIdentityExamples.Controllers
 {
     public class HomeController : Controller
     {
+       
+
         // GET: /<controller>/
+        [Authorize]
         public IActionResult Index()
         {
             return View(new Dictionary<string, object> {

bb4ce4eb13ddc70e2a127e0c5e28de0d10610ac0
darren <764190362@qq.com>
2017-10-06 16:13:57 +0800

    角色增删改

---
+++ Infrastructure/RoleUsersTagHelper.cs
@@ -0,0 +1,47 @@
+﻿using AspNetCoreIdentityExamples.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreIdentityExamples.Infrastructure
+{
+    [HtmlTargetElement("td", Attributes = "identity-role")]
+    public class RoleUsersTagHelper:TagHelper
+    {
+        private UserManager<AppUser> userManager;
+        private RoleManager<IdentityRole> roleManager;
+
+        public RoleUsersTagHelper(UserManager<AppUser> usermgr, RoleManager<IdentityRole> rolemgr)
+        {
+            userManager = usermgr;
+            roleManager = rolemgr;
+        }
+
+        //这里的Role的属性值实际是Role的主键
+        [HtmlAttributeName("identity-role")]
+        public string Role { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            List<string> userNames = new List<string>();
+            IdentityRole role = await roleManager.FindByIdAsync(Role);
+            if (role != null)
+            {
+                foreach(var user in userManager.Users)
+                {
+                    //判断用户是否属于某个角色
+                    if(user != null && await userManager.IsInRoleAsync(user, role.Name))
+                    {
+                        userNames.Add(user.UserName);
+                    }
+                }
+            }
+
+            output.Content.SetContent(userNames.Count == 0 ? "No Users": string.Join(", ", userNames));
+        }
+    }
+}

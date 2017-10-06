a673ab7b87b712ad4bf72a78969e1b8e98eff105
darren <764190362@qq.com>
2017-10-06 12:51:37 +0800

    自定义PasswordValidator

--- Infrastructure/CustomPasswordValidator.cs
+++ Infrastructure/CustomPasswordValidator.cs
@@ -7,19 +7,50 @@ using System.Threading.Tasks;
 
 namespace AspNetCoreIdentityExamples.Infrastructure
 {
-    public class CustomPasswordValidator : IPasswordValidator<AppUser>
-    {
-       
+    #region 1
+    //public class CustomPasswordValidator : IPasswordValidator<AppUser>
+    //{
 
-       
-        public Task<IdentityResult> ValidateAsync(UserManager<AppUser> manager, AppUser user, string password)
+
+
+    //    public Task<IdentityResult> ValidateAsync(UserManager<AppUser> manager, AppUser user, string password)
+    //    {
+    //        List<IdentityError> errors = new List<IdentityError>();
+    //        if (password.ToLower().Contains(user.UserName.ToLower()))
+    //        {
+    //            errors.Add(new IdentityError
+    //            {
+    //                Code = "PasswordContainUserName",
+    //                Description = "Passowrd cannot contain username"
+    //            });
+    //        }
+
+    //        if (password.Contains("12345"))
+    //        {
+    //            errors.Add(new IdentityError
+    //            {
+    //                Code = "PasswordContainsSequence",
+    //                Description = "Password cannot contain numeric swquence"
+    //            });
+    //        }
+
+    //        return Task.FromResult(errors.Count == 0 ? IdentityResult.Success : IdentityResult.Failed(errors.ToArray()));
+    //    }
+    //} 
+    #endregion
+
+    public class CustomPasswordValidator : PasswordValidator<AppUser>
+    {
+        public override async Task<IdentityResult> ValidateAsync(UserManager<AppUser> manager, AppUser user, string password)
         {
-            List<IdentityError> errors = new List<IdentityError>();
+            IdentityResult result = await base.ValidateAsync(manager, user, password);
+            List<IdentityError> errors = result.Succeeded ? new List<IdentityError>() : result.Errors.ToList();
+
             if (password.ToLower().Contains(user.UserName.ToLower()))
             {
                 errors.Add(new IdentityError {
-                    Code = "PasswordContainUserName",
-                    Description="Passowrd cannot contain username"
+                    Code = "PasswordContainsUserName",
+                    Description="Password cannot contain username"
                 });
             }
 
@@ -27,11 +58,11 @@ namespace AspNetCoreIdentityExamples.Infrastructure
             {
                 errors.Add(new IdentityError {
                     Code = "PasswordContainsSequence",
-                    Description="Password cannot contain numeric swquence"
+                    Description="Password cannto contain numeric sequence"
                 });
             }
 
-            return Task.FromResult(errors.Count == 0 ? IdentityResult.Success : IdentityResult.Failed(errors.ToArray()));
+            return errors.Count == 0 ? IdentityResult.Success : IdentityResult.Failed(errors.ToArray());
         }
     }
 }
